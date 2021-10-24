13f455246069e15c1acde88ba963d3f5305b9b99
Yi Lin <spimtalin@gmail.com>
2021-10-24 19:17:06 -0400

    add comments

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ dim_x = 10
 dim_y = 10
 
 grid = np.zeros((dim_x, dim_y))
+
+# define exits
 exit_cells = frozenset(((dim_x // 2-1, dim_y - 1), (dim_x // 2, dim_y - 1),
                         (dim_x - 1, dim_y//2) , (dim_x - 1, dim_y//2-1),
                         (0, dim_y//2 -1) , (0, dim_y//2),
@@ -13,8 +15,11 @@ exit_cells = frozenset(((dim_x // 2-1, dim_y - 1), (dim_x // 2, dim_y - 1),
 print(np.zeros((10, 10), float))
 
 
+# initialize walls to be 500
 def init_walls(exit_cells):
     grid[0, :] = grid[-1, :] = grid[:, -1] = grid[:, 0] = 500
+
+    # initialize exit
     for e in exit_cells:
         grid[e] = -1
 
@@ -23,7 +28,7 @@ init_walls(exit_cells)
 
 print(grid)
 
-
+# get diagonal neighbors of a cell, return a list of cells
 def get_diag_neighbors(cell):
     neighbors = []
     i, j = cell
@@ -37,11 +42,12 @@ def get_diag_neighbors(cell):
         neighbors.append((i - 1, j + 1))
     return neighbors
 
-
+# get neighbors of a cell, default to be von Neumann neighborhood, if second argument = 1, then get moore neighbor
+# return a list of cells
 def get_neighbors(cell, moore=0):
-    """
-     von Neumann neighborhood
-    """
+
+    # von Neumann neighborhood
+
     neighbors = []
     i, j = cell
     if i < dim_x - 1 and grid[(i + 1, j)] != 500:
@@ -67,15 +73,17 @@ def get_neighbors(cell, moore=0):
     return neighbors
 
 
+# initial static floor field
 def init_sff(exit_cells):
     for e in exit_cells:
         e_neighbor = get_neighbors(e)
-        print(e_neighbor)
+        # print(e_neighbor)
         for c in e_neighbor:
             if c not in exit_cells:
                 init_sff_rec(c, 1)
 
 
+# a recursive function to initialize static floor field
 def init_sff_rec(_cell, _value):
     global grid
     grid[_cell] = _value
