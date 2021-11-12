499df5a6a25c29a35f38945dd4ec586d1a31c1a4
name <shuoxinliu961029@gmail.com>
2021-11-12 03:46:10 -0500

    animation fixed

--- main.py
+++ main.py
@@ -5,48 +5,34 @@ import numpy as np
 import random
 from math import sqrt
 
-dim_x = 20
-dim_y = 20
-rf = 2  # current fire range
-rm = 6  # maximum fire range
+dim_x = 10
+dim_y = 100
+
 tf = 10  # fire evolution frequency
-w = 1.05  # w is a weight for I in the papaer
+w = 1.05  # w is a weight for I in the paper
 ks = 0.5
 kd = 0.2
-kf = 0.3
-time = 0
+kf = 0.1
 alpha = 0.2  # coeffision for diffusion
 delta = 0.2  # coeefision for decay
 myLambda = 0.50  # Sedation probability increase coefficient
 
-pedestrain_matrix = np.zeros((dim_x, dim_y))
 pedestrains = []
+visual_field = np.zeros((dim_x, dim_y))
 sff = np.zeros((dim_x, dim_y))
 dff = np.zeros((dim_x, dim_y))  # dynamic floor field
 dff_diff = np.zeros(
     (dim_x, dim_y))  # dff difference matrix, update current grid then add to dff through update_dff function
 
-# define exits
-exit_cells = frozenset((
-    (dim_x // 2 - 1, dim_y - 1), (dim_x // 2, dim_y - 1),
-    (dim_x - 1, dim_y // 2), (dim_x - 1, dim_y // 2 - 1),
-    (0, dim_y // 2 - 1), (0, dim_y // 2),
-    (dim_x // 2 - 1, 0), (dim_x // 2, 0),
-    # (0,4), (0,5)
-))
-# print(exit_cells)
-
-fire_cells = {(4, 4), (4, 5), (5, 4), (5, 5)}
-
 
 # initialize walls to be 500
 def init_walls(exit_cells):
     sff[0, :] = sff[-1, :] = sff[:, -1] = sff[:, 0] = 500
-    pedestrain_matrix[0, :] = pedestrain_matrix[-1, :] = pedestrain_matrix[:, -1] = pedestrain_matrix[:, 0] = 500
+    visual_field[0, :] = visual_field[-1, :] = visual_field[:, -1] = visual_field[:, 0] = 500
     # initialize exit
     for e in exit_cells:
         sff[e] = 0.1
-        pedestrain_matrix[e] = 0
+        visual_field[e] = 0
 
 
 # get diagonal neighbors of a cell, return a list of cells
@@ -66,33 +52,33 @@ def get_diag_neighbors(cell):
 
 # get neighbors of a cell, default to be von Neumann neighborhood, if second argument = 1, then get moore neighbor
 # return a list of cells
-def get_neighbors(cell, moore=0):
+def get_neighbors(cell, moore=0, ignore_val=500):
     # von Neumann neighborhood
     neighbors = []
     i, j = cell
-    if i < dim_x - 1 and sff[(i + 1, j)] != 500:
+    if i < dim_x - 1 and sff[(i + 1, j)] != ignore_val:
         neighbors.append((i + 1, j))
-    if i >= 1 and sff[(i - 1, j)] != 500:
+    if i >= 1 and sff[(i - 1, j)] != ignore_val:
         neighbors.append((i - 1, j))
-    if j < dim_y - 1 and sff[(i, j + 1)] != 500:
+    if j < dim_y - 1 and sff[(i, j + 1)] != ignore_val:
         neighbors.append((i, j + 1))
-    if j >= 1 and sff[(i, j - 1)] != 500:
+    if j >= 1 and sff[(i, j - 1)] != ignore_val:
         neighbors.append((i, j - 1))
 
     # moore
     if moore:
-        if i >= 1 and j >= 1 and sff[(i - 1, j - 1)] != 500:
+        if i >= 1 and j >= 1 and sff[(i - 1, j - 1)] != ignore_val:
             neighbors.append((i - 1, j - 1))
-        if i < dim_x - 1 and j < dim_y - 1 and sff[(i + 1, j + 1)] != 500:
+        if i < dim_x - 1 and j < dim_y - 1 and sff[(i + 1, j + 1)] != ignore_val:
             neighbors.append((i + 1, j + 1))
-        if i < dim_x - 1 and j >= 1 and sff[(i + 1, j - 1)] != 500:
+        if i < dim_x - 1 and j >= 1 and sff[(i + 1, j - 1)] != ignore_val:
             neighbors.append((i + 1, j - 1))
-        if i >= 1 and j < dim_y - 1 and sff[(i - 1, j + 1)] != 500:
+        if i >= 1 and j < dim_y - 1 and sff[(i - 1, j + 1)] != ignore_val:
             neighbors.append((i - 1, j + 1))
     return neighbors
 
 
-def get_neighbors_ignore_wall(cell, moore=0):
+def get_neighbors_including_wall(cell, moore=0):
     # von Neumann neighborhood
     neighbors = []
     i, j = cell
@@ -163,7 +149,6 @@ def init_dff_diff():
 def update_dff():
     global dff, dff_diff
     dff += dff_diff
-
     # iter through all cells in the grid
     for i, j in itertools.chain(itertools.product(range(1, dim_x - 1), range(1, dim_y - 1)), exit_cells):
         for _ in range(int(dff[i, j])):
@@ -178,7 +163,7 @@ def update_dff():
 def update_fire():
     '''todo: further add more rules'''
     for i in fire_cells:
-        pedestrain_matrix[i] = 499
+        visual_field[i] = 499
 
 
 # fire evolution by tf
@@ -186,16 +171,15 @@ def fire_evolution(t):
     global rf, tf, fire_cells
 
     if t == 0:
+        update_fire()
         return
 
     tmp = set()
-    if t % tf == 0 and rf != rm:
-        rf = rf + 2
+    if t % tf == 0:
         for i in fire_cells:
-            neighbors = get_neighbors(i, 1)
+            neighbors = get_neighbors(i, 1, 0)
             for j in neighbors:
                 tmp.add(j)
-
     fire_cells = fire_cells.union(tmp)
     update_fire()
 
@@ -220,10 +204,8 @@ class Rectangle:  # [A,B]
         # check range
         co = self.all_coordinates()
         for i in co:
-            if i in fire_cells:
-                raise Exception("Fire cells included")
-            if i[0] == 0 or i[1] == 0:
-                raise Exception("Wall cells included")
+            if i[0] == 0 or i[1] == 0 or i[0] == dim_x-1 or i[1] == dim_y-1:
+                raise Exception("Wall cells {} included".format(i))
 
     def complete(self):
         return self.x, self.y, self.w, self.h
@@ -233,36 +215,33 @@ class Rectangle:  # [A,B]
                 (self.x + self.w, self.y - self.h)]
 
     def all_coordinates(self):  # return all coordinates
-        return list(itertools.product(range(self.x, self.x + self.w + 1), range(self.y - self.h, self.y + 1)))
+        return list(itertools.product(range(self.x, self.x + self.w + 1), range(self.y, self.y + self.h+1)))
 
-intend_list = []
+
 class Pedestrain:
     def __init__(self, coord):
         if coord[0] == 0 or coord[1] == 0 or coord[0]==dim_x-1 or coord[1]==dim_y-1:
             raise Exception("Wall cells included")
-        if coord in fire_cells:
-            raise  Exception("Fire cells included")
-        self.exited = 0  # 1 if pedestrain exited successfuly
-        if coord in fire_cells:
-            raise Exception("Fire cells included")
-        if coord in fire_cells:
-            raise Exception("Fire cells included")
+        self.status = 0  # 1 if pedestrain exited successfuly, 2 if pedestrain died in the fire
         self.now = coord
         self.update_xy()
         if self.x == 0 or self.y == 0:
             raise Exception("Wall cells included")
-        if pedestrain_matrix[coord] == 999:
+        if visual_field[coord] == 999:
             raise Exception("There is already a pedestrain at {}".format(self.now))
         self.last = coord  # last position
-        pedestrain_matrix[coord] = 999
+        visual_field[coord] = 999
 
     def step(self):
         print("\nPedstrain: ", self.now)
         if self.now in exit_cells: # exit successfully
-            self.exited = 1
-            pedestrain_matrix[self.now] = 0
+            self.status = 1
+            visual_field[self.now] = 0
+        elif self.now in fire_cells: # died
+            self.status = 2
+            visual_field[self.now] = 1000
         else:
-            pedestrain_matrix[self.now] = 0
+            visual_field[self.now] = 0
             if self.last != self.now:
                 dff_diff[self.last] += 1
             max = np.max(self.P)
@@ -270,20 +249,23 @@ class Pedestrain:
             dir = (max_index[0][0] - 1, max_index[1][0] - 1)
             self.last = self.now
             temp = tuple(self.now + np.array(dir))
-            while temp in intend_list:
+            while temp in occupied_cells or temp in fire_cells:
                 self.P[self.P == max] = 0
                 max = np.max(self.P)
                 max_index = np.where(self.P == max)
                 dir = (max_index[0][0] - 1, max_index[1][0] - 1)
                 temp = tuple(self.now + np.array(dir))
+                # print("\nPedestrain: ", self.last, self.now, "\n S:\n", self.get_S(), "\n I:\n", self.I, "\n n:\n",
+                #       self.n, "\n epsilon:\n", self.epsilon,
+                #       "\n F:\n", self.F, "\n", self.P)
             self.now = temp
             self.update_xy()
-            intend_list.append(self.now)
-            pedestrain_matrix[self.now] = 999
+            occupied_cells.append(self.now)
+            visual_field[self.now] = 999
             print(self.now)
 
     def update(self):
-        if self.now not in exit_cells:
+        if self.now not in exit_cells and self.now not in fire_cells:
             self.update_I()
             self.update_F()
             self.update_epsilon()
@@ -302,7 +284,7 @@ class Pedestrain:
     @staticmethod
     def update_cell():
         for i in pedestrains:
-            pedestrain_matrix[i.now] = 999
+            visual_field[i.now] = 999
 
     def __str__(self):
         return str(self.now)
@@ -335,20 +317,21 @@ class Pedestrain:
 
     def update_F(self):  # fire floor field, 0 means out of range
         self.F = np.zeros((3, 3))
-        neighbors = get_neighbors_ignore_wall(self.now, 1)
+        neighbors = get_neighbors_including_wall(self.now, 1)
         x = self.x - 1
         y = self.y - 1
         neighbors.append(self.now)
         for i in neighbors:
             self.F[i[0] - x, i[1] - y] = self.compute_H(i)
-        self.F = 1 / self.F
+        print("F____________", self.F)
+        self.F = np.where(self.F == 0, 999999,  1 / self.F) # Fire field overlap with fire cells, to avoid 1/0 error ,set it to 0.01
+        print("F____________", self.F)
         sum = 0
-        for i in self.F:
+        for i in self.F.flatten():
             sum += i
         self.F = self.F / sum
 
     def get_F(self):
-        print(self.F)
         return self.F
 
     def compute_H(self, coord):
@@ -370,7 +353,6 @@ class Pedestrain:
         self.H = temp
 
     def get_H(self):
-        print("H:", self.H)
         return self.H
 
     def get_closet_fire(self):
@@ -380,21 +362,19 @@ class Pedestrain:
         self.Pc = 1 - math.exp(-myLambda * (self.H - 1))
 
     def get_Pc(self):
-        print("Pc：", self.Pc)
         return self.Pc
 
     def update_epsilon(self):  # update obstacle matrix,  epsilon is [] when pedestrian on the border/exit
-        temp = pedestrain_matrix[self.x - 1:self.x + 2, self.y - 1:self.y + 2]
+        temp = visual_field[self.x - 1:self.x + 2, self.y - 1:self.y + 2]
         self.epsilon = np.where(temp == 500, 0, 1)
         if self.epsilon.size == 0:  # epsilon is [] when pedestrian on the border/exit
             self.epsilon = numpy.zeros((3, 3))
 
     def get_epsilon(self):
-        print("Epsilon:", self.epsilon, self.n)
         return self.epsilon
 
     def update_n(self):  # update target cell occupation matrix, n is [] when pedestrian on the border/exit
-        temp = pedestrain_matrix[self.x - 1:self.x + 2, self.y - 1:self.y + 2]
+        temp = visual_field[self.x - 1:self.x + 2, self.y - 1:self.y + 2]
         # print(temp)
         self.n = np.where(temp == 999, 1, 0)
         if self.n.size != 0:  # n is [] when pedestrian on the border/exit
@@ -416,20 +396,19 @@ class Pedestrain:
         d = dff[self.x - 1:self.x + 2, self.y - 1:self.y + 2]
         return d
 
-    # def burned(self):
-    #
-
 
 def generate_pedestrain_rand(num, rectangle):
     pedestrain_cells = random.sample(rectangle.all_coordinates(), num)
     for i in pedestrain_cells:
-        pedestrains.append(Pedestrain(i))
+        if visual_field[i] == 0:
+            pedestrains.append(Pedestrain(i))
 
 
 def generate_pedestrain(x):  # x is a tuple|Rectangle
     if isinstance(x, Rectangle):
         for i in x.all_coordinates():
-            pedestrains.append(Pedestrain(i))
+            if visual_field[i] == 0:
+                pedestrains.append(Pedestrain(i))
     if isinstance(x, tuple):
         if x in fire_cells:
             raise Exception("Fire cells included")
@@ -442,15 +421,12 @@ import random
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap, BoundaryNorm
 import matplotlib.animation as animation
-import copy
 
-
-def Update(frameNum, img, oldGrid, nC):
-    one_step()
-    newGrid = pedestrain_matrix
-    displayGrid = copy.deepcopy(newGrid)
-    img.set_data(displayGrid)
-    oldGrid[:, :] = newGrid[:, :]
+def Update(frame, img,):
+    if frame == 0: # skip 0
+        return img
+    one_step(frame)
+    img.set_data(visual_field)
     return img
 
 
@@ -459,99 +435,65 @@ def animate():
     green = np.array([50 / 256, 200 / 256, 50 / 256, 1])
     red = np.array([255 / 256, 90 / 256, 90 / 256, 1])
     coffee = np.array([111 / 256, 78 / 256, 55 / 256, 1])
-    newColors = np.zeros((4, 4))
+    black = np.array([0, 0, 0, 1])
+    newColors = np.zeros((5, 4))
     newColors[0, :] = light_gray
     newColors[1, :] = red
     newColors[2, :] = coffee
     newColors[3, :] = green
-    print(newColors)
+    newColors[4, :] = black
+
     Cmap = ListedColormap(newColors)
-    boundary_norm = BoundaryNorm([-0.5, 498.5, 499.5, 500.5, 999.5], Cmap.N)
+    boundary_norm = BoundaryNorm([-0.5, 498.5, 499.5, 500.5, 999.5, 1000.5], Cmap.N)
 
     fig, ax = plt.subplots()
     ax.axis('off')
+    img = ax.imshow(visual_field, cmap=Cmap, interpolation='nearest', norm=boundary_norm)
 
-    oldGrid = pedestrain_matrix
-
-    img = ax.imshow(oldGrid, cmap=Cmap, interpolation='nearest', norm=boundary_norm)
-    ani = animation.FuncAnimation(fig, Update, fargs=(img, oldGrid, 10),
-                                  frames=128,
-                                  interval=200,
+    ani = animation.FuncAnimation(fig, Update, fargs=(img, ), init_func=init,
+                                  frames=200,
+                                  interval=300,
                                   repeat=False)
     f = r'.\test.mp4'
     ani.save(f, writer='ffmpeg', fps=1)
 
 
-# generate_pedestrain_rand(1, rec)
-# print(sff)
-# print(pedestrain_matrix)
-# for i in pedestrains:
-#     print("pedestrains:")
-#     print(i)
 def init():
+    global fire_cells, exit_cells
+    # define exits
+    exit_cells = frozenset((
+        (dim_x // 2 - 1, dim_y - 1), (dim_x // 2, dim_y - 1),
+        # (dim_x - 1, dim_y // 2), (dim_x - 1, dim_y // 2 - 1),
+        # (0, dim_y // 2 - 1), (0, dim_y // 2),
+        # (dim_x // 2 - 1, 0), (dim_x // 2, 0),
+    ))
+    # fire_cells = {(4, 4), (4, 5), (5, 4), (5, 5)}
+    fire_cells = {(4, 50)}
     init_walls(exit_cells)
     init_sff(exit_cells)
-    update_fire()
+    #Assign pedestrains
+    rec = Rectangle(1, 1, 7, 97)
+    generate_pedestrain_rand(50, rec)
 
 
-# generate_pedestrain((9, 9))
-# print(pedestrain_matrix)
-
-def one_step():
-    global time, pedestrains, pedestrain_matrix, intend_list
+def one_step(time):
+    global pedestrains, visual_field, occupied_cells
+    occupied_cells = []
     print("\n-----------------------------", time)
+    fire_evolution(time)
     for i in pedestrains:
-        if i.exited == 0:
+        if i.status == 0:
             i.update()
-    for i in pedestrains:
-        if i.exited == 0:
             i.step()
-            print(pedestrain_matrix)
-    intend_list = []
+            print(visual_field)
+    # for i in pedestrains:
+    #     if i.status == 0:
+    #         i.step()
+    #         print(visual_field)
     update_dff()
     init_dff_diff()
-    time += 1
-    fire_evolution(time)
 
 
-init()
-rec = Rectangle(1, 2, 1, 1)
-generate_pedestrain(rec)
-rec = Rectangle(10, 10, 7, 7)
-generate_pedestrain_rand(20,rec)
-animate()
-# for i in range(4):
+# for i in range(20):
 #     one_step()
-
-
-# print(pedestrain_matrix)
-# me = Pedestrain((1, 1))
-# pedestrains.append(me)
-# print(pedestrain_matrix)
-# # me.get_I()
-# # me.get_H()
-# # me.get_Pc()
-# # me.get_epsilon()
-# me.step()
-# # me.get_I()
-# # me.get_H()
-# # me.get_Pc()
-# # me.get_epsilon()
-# print(pedestrain_matrix)
-#
-# me.step()
-# # me.get_I()
-# # me.get_H()
-# # me.get_Pc()
-# # me.get_epsilon()
-# print(pedestrain_matrix)
-#
-# me.step()
-# # me.get_I()
-# # me.get_H()
-# # me.get_Pc()
-# # me.get_epsilon()
-# print(pedestrain_matrix)
-
-
-# animate()
+animate()
